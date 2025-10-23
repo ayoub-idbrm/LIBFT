27fc959b41fd0a0b3ba5b5019a03b3c07e7ccd1f
Ayoub Idbrm <aidbrm@e1r9p4.1337.ma>
2025-10-23 01:17:53 +0100

    test

--- ft_isalnum.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-int	ft_isalnum(int c)
-{
-	if (c >= 'a' && c <= 'z' || c >= '0' && c <= '9' || c >= 'A' && c <= 'z')
-		return (1);
-	else 
-		return (0);
-}
--- ft_tolower.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-int	ft_tolower(int c)
-{
-	 if (c >= 'a' && c <= 'z')
-        {
-                c -= 32;
-        }
-        return (c);
-}
